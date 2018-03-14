5be8ff7f6007877acffe5ac3b6823d78961c3be7
Jason-Iverson.com <contact@jason-iverson.com>
2018-03-13 17:44:14 -0700

    New user defined function

---
+++ utility/Functions/fnToProperCase.sql
@@ -0,0 +1,42 @@
+﻿/*
+Function adapted from this StackOverflow.com post
+https://stackoverflow.com/a/230224
+*/
+
+CREATE FUNCTION fnToProperCase(@string nvarchar(max)) RETURNS nvarchar(max)
+AS
+BEGIN
+  DECLARE @i INT           -- index
+  DECLARE @l INT           -- input length
+  DECLARE @c NCHAR(1)      -- current char
+  DECLARE @f INT           -- first letter flag (1/0)
+  DECLARE @o nvarchar(max) -- output string
+  DECLARE @w nvarchar(10)   -- characters considered as white space
+
+  SET @w = N'[' + CHAR(13) + CHAR(10) + CHAR(9) + CHAR(160) + ' ' + ']'
+  SET @i = 1
+  SET @l = LEN(@string)
+  SET @f = 1
+  SET @o = ''
+
+  WHILE @i <= @l
+  BEGIN
+    SET @c = SUBSTRING(@string, @i, 1)
+    IF @f = 1 
+    BEGIN
+     SET @o = @o + @c
+     SET @f = 0
+    END
+    ELSE
+    BEGIN
+     SET @o = @o + LOWER(@c)
+    END
+
+    IF @c LIKE @w SET @f = 1
+
+    SET @i = @i + 1
+  END
+
+  RETURN @o
+END
+GO
